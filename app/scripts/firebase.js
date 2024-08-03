04e030958e659227b7cf8ee2e21735d7075b7586
The Myth <dagamudith@gmail.com>
2024-08-04 02:32:00 +0530

    Firebase

--- app/scripts/firebase.js
+++ app/scripts/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,4 +20,53 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);   
+const db = getFirestore(app);
+
+function emailExists(email) {
+    if (db.collection("users").onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            if (doc.data().email == email) return true
+        });
+    }));
+    return false;
+}
+
+async function signUp(){
+    var name = document.getElementById("name").value,
+        email = document.getElementById("email").value,
+        password = document.getElementById("password").value;
+    if (emailExists(email)) {
+        alert("Email already exists");
+        return;
+    }
+
+    await setDoc(doc(db, "cities", ), {
+        name: name,
+        email: email,
+        password: password
+    });
+
+    alert("Sign up successful");
+    window.location.href = "home.html";
+}
+
+async function signIn(){
+    var email = document.getElementById("email").value,
+        password = document.getElementById("password").value;
+
+    if (!emailExists){
+        alert("Email does not exist");
+        return;
+    }
+
+    if (db.collection("users").onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            if (doc.data().email == email && doc.data().password == password) {
+                alert("Sign in successful");
+                window.location.href = "home.html";
+            }
+        });
+    }));
+
+    alert("Invalid email or password");
+}
